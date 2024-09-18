6342df1a3017c9859aa5a3ff61b7dbdee64bffa9
lianecc <liane.coupat@gmail.com>
2024-09-18 18:32:54 +0200

    update modale ajout-suppression projets

--- FrontEnd/connexion.js
+++ FrontEnd/connexion.js
@@ -2,16 +2,13 @@ async function ajoutListenerConnexion() {
     const formulaireConnexion = document.querySelector("#formulaire-login");
     formulaireConnexion.addEventListener("submit", async (event) => {
             event.preventDefault();
-
             // Objet de connexion
             const connexion = {
                 email: document.querySelector("#email").value,
                 password: document.querySelector("#password").value
             };
-
             // Création charge utile 
             const chargeUtile = JSON.stringify(connexion);
-
             // fetch 
             const reponse = await fetch("http://localhost:5678/api/users/login", {
                 method: "POST",
@@ -41,73 +38,5 @@ function afficherMessageErreur(message) {
 
 ajoutListenerConnexion();
 
-
-export function afficherBoutonConnexion() {
-    const boutonConnexion = document.querySelector("#login-logout");
-    if (localStorage.getItem('auth-token')) {
-        // connexion validée, changement du texte
-        boutonConnexion.textContent = "logout" ;
-        boutonConnexion.setAttribute("href", "#");
-        // affichage bannerEditionMode 
-        const bannerEditionMode = document.querySelector(".bannerEditionMode");
-        bannerEditionMode.style.display = "flex";
-        
-        // Suppression des boutons
-        const boutons = document.querySelectorAll(".boutons button");
-        boutons.forEach(bouton => {
-            bouton.remove();
-        });
-
-        // affichage encart Modifier à côté de Mes Projets
-        const bannerModifier = document.querySelector(".bannerModifier");
-        bannerModifier.style.display = "flex";
-
-        // gestion modale pour ajout projet 
-        let modale = null; 
-
-        // ouverture de la modale
-        const openModal = function (event) {
-            event.preventDefault;
-            const target = document.querySelector(event.target.getAttribute("href"));
-            target.style.display = null;
-            target.removeAttribute("aria-hidden");
-            target.setAttribute("aria-modal", "true");
-            modale = target;
-            modale.addEventListener("click", closeModal);
-            modale.querySelector(".fermerModale").addEventListener("click", closeModal);
-            modale.querySelector(".modal-stop").addEventListener("click", stopPropagation);
-        }
-
-        
-        // fermeture de la modale
-        const closeModal = function (event) {
-            if (modale === null) return;
-            event.preventDefault()
-            modale.style.display="none";
-            modale.setAttribute("aria-hidden", "true");
-            modale.removeAttribute("aria-modal");
-            modale.removeEventListener("click", closeModal);
-            modale.querySelector(".fermerModale").removeEventListener("click", closeModal);
-            modale.querySelector(".modal-stop").removeEventListener("click", stopPropagation);
-            modale = null;
-        }
-        const stopPropagation = function (event) {
-            event.stopPropagation();            
-        }
-
-        // appel de la fonction ouverture modale
-        document.querySelectorAll(".boutonModifier").forEach(a => {
-            a.addEventListener("click", openModal);
-        })
-
-        // listener pour deconnexion utilisateur
-        boutonConnexion.addEventListener("click", () => {
-            localStorage.removeItem('auth-token');
-            window.location.href = "index.html";
-        });
-    } 
-}
-
-afficherBoutonConnexion();
 
 
--- FrontEnd/projets.js
+++ FrontEnd/projets.js
@@ -1,9 +1,41 @@
-import { afficherBoutonConnexion } from "./connexion.js";
+// ****** CONNEXION UTILISATEUR 
+function afficherBoutonConnexion() {
+    const boutonConnexion = document.querySelector("#login-logout");
+    if (localStorage.getItem('auth-token')) {
+        // connexion validée, changement du texte
+        boutonConnexion.textContent = "logout" ;
+        boutonConnexion.setAttribute("href", "#");
+        // affichage bannerEditionMode 
+        const bannerEditionMode = document.querySelector(".bannerEditionMode");
+        bannerEditionMode.style.display = "flex";
+        
+        // Suppression des boutons
+        const boutons = document.querySelectorAll(".boutons button");
+        boutons.forEach(bouton => {
+            bouton.remove();
+        });
 
+        // listener pour deconnexion utilisateur
+        boutonConnexion.addEventListener("click", () => {
+            localStorage.removeItem('auth-token');
+            window.location.href = "index.html";
+        });
+
+        // affichage encart Modifier à côté de Mes Projets
+        const bannerModifier = document.querySelector(".bannerModifier");
+        bannerModifier.style.display = "flex";
+    } 
+}
+
+//appel fonction connexion
 window.onload = () => {
     afficherBoutonConnexion();
 };
 
+
+
+
+// ***** AFFICHAGE PROJET SUR PAGE D'ACCUEIL
 // Récupération des projets réalisés depuis l'API
 const reponseProjets = await fetch('http://localhost:5678/api/works');
 const projets = await reponseProjets.json();
@@ -33,13 +65,12 @@ function genererProjet(projets) {
 // Affiche tous les projets au chargement
 genererProjet(projets);
 
-
+// GESTION DES BOUTONS FILTRES 
 // enlever 'selected' de tous les boutons
 function retirerSelectionBoutons() {
     const boutons = document.querySelectorAll(".boutons button");
     boutons.forEach(bouton => bouton.classList.remove("selected"));
 }
-
 
 // Récupération des catégories depuis l'API
 const reponseCategories = await fetch('http://localhost:5678/api/categories');
@@ -60,7 +91,6 @@ function genererBouton(categories) {
         // Ajout d'un EventListener pour afficher tous les projets
         boutonTous.addEventListener("click", () => {
             genererProjet(projets);
-
             // update du style du bouton
             retirerSelectionBoutons();
             boutonTous.classList.add("selected");
@@ -78,7 +108,6 @@ function genererBouton(categories) {
                 const projetsFiltres = projets.filter(projet => projet.categoryId === categorie.id);
                 document.querySelector(".gallery").innerHTML = "";
                 genererProjet(projetsFiltres);
-
                 // update du style du bouton
                 retirerSelectionBoutons();
                 boutonElement.classList.add("selected");
@@ -89,6 +118,64 @@ function genererBouton(categories) {
         }
     }
 }
-
+//appel fonction création boutons
 genererBouton(categories);
 
+
+// GESTION MODALE POUR AJOUT/SUPPRESSION PROJETS
+let modale = null; 
+
+// ouverture de la modale
+const openModal = function (event) {
+    event.preventDefault;
+    const target = document.querySelector(event.target.getAttribute("href"));
+    target.style.display = null;
+    target.removeAttribute("aria-hidden");
+    target.setAttribute("aria-modal", "true");
+    modale = target;
+    modale.addEventListener("click", closeModal);
+    modale.querySelector(".fermerModale").addEventListener("click", closeModal);
+    modale.querySelector(".modal-stop").addEventListener("click", stopPropagation);
+}
+
+// fermeture de la modale
+const closeModal = function (event) {
+    if (modale === null) return;
+    event.preventDefault()
+    modale.style.display="none";
+    modale.setAttribute("aria-hidden", "true");
+    modale.removeAttribute("aria-modal");
+    modale.removeEventListener("click", closeModal);
+    modale.querySelector(".fermerModale").removeEventListener("click", closeModal);
+    modale.querySelector(".modal-stop").removeEventListener("click", stopPropagation);
+    modale = null;
+}
+//limiter la fermeture de la modale aux éléments enfants
+const stopPropagation = function (event) {
+    event.stopPropagation();            
+}
+
+// ajout des projets dans la modale 
+function genererProjetModale(projets) {
+    const sectionProjetModale = document.querySelector(".projets-disponibles");
+    
+    for (let i = 0; i <projets.length; i++) {
+        const projetModale = projets[i];
+        const imageProjetModale = document.createElement("img");
+        imageProjetModale.src = projetModale.imageUrl;
+        sectionProjetModale.appendChild(imageProjetModale);
+    }
+}
+genererProjetModale(projets);
+
+// ouverture modale depuis le bouton
+document.querySelectorAll(".boutonModifier").forEach(a => {
+    a.addEventListener("click", openModal);
+})
+
+// changement de vue modale selon bouton
+// de basse, retourvue1 est en display none 
+//si on clique sur ajouter photo, on bascule sur vuemodale2 (display: null)
+// et on met vuemodale1 en display none 
+// si on clique sur la fleche retour vue 1, on rebascule sur vuemodale1 display null et vuemodale2 display none 
+//  
